cf28c42632f5622aecc9071fa113bd265c14a069
Encryptabit <jacari.jones@live.com>
2022-08-12 18:56:52 -0700

    Update Remote Alerts
    Update Remote Alerts to be both hosted and standalone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotificationPreferencesModule } from './components/notification-preferences/notification-preferences.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NotificationPreferencesModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,16 @@
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
-const mf = require("@angular-architects/module-federation/webpack");
-const path = require("path");
-const share = mf.share;
+const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
-const sharedMappings = new mf.SharedMappings();
-sharedMappings.register(
-  path.join(__dirname, 'tsconfig.json'),
-  [/* mapped paths to share */]);
+module.exports = withModuleFederationPlugin({
 
-module.exports = {
-  output: {
-    uniqueName: "websiteRemoteAlerts",
-    publicPath: "http://localhost:1338/",
-    scriptType: 'text/javascript'
-  },
-  optimization: {
-    runtimeChunk: false
-  },   
-  resolve: {
-    alias: {
-      ...sharedMappings.getAliases(),
-    }
-  },
-  experiments: {
-    outputModule: true
-  },
-  plugins: [
-    new ModuleFederationPlugin({
-       // For remotes (please adjust)
-        name: "notificationPreferences",
-        filename: "remoteEntry.js",
-        exposes: {
-            './Module': './/src/app/components/notification-preferences/notification-preferences.module.ts'
-        },
-        shared: share({
-          "@angular/core": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
-          "@angular/common": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
-          "@angular/common/http": { singleton: true, strictVersion: true, requiredVersion: 'auto' }, 
-          "@angular/router": { singleton: true, strictVersion: true, requiredVersion: 'auto' },
+  name: 'notification-preferences',
 
-          ...sharedMappings.getDescriptors()
-        })
-        
-    }),
-    sharedMappings.getPlugin()
-  ],
-};
+  exposes: {
+    './Module': './/src/app/components/notification-preferences/notification-preferences.module.ts',
+  },
+
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  },
+
+});
+
